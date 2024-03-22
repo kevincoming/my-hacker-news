cb3a1389a4b4a557be003521525c61508afaa9c9
phanchithien <phanchithien456@gmail.com>
2024-03-22 15:31:22 +0700

    upgrade

---
+++ src/InputWithLabel.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+const InputWithLabel = ({
+    id,
+    value,
+    type = 'text',
+    onInputChange,
+    children,
+}) => (
+    <>
+        <label htmlFor={id}>{children}</label>
+        &nbsp;
+        <input
+            id={id}
+            type={type}
+            value={value}
+            onChange={onInputChange}
+        />
+    </>
+);
+
+export { InputWithLabel }
